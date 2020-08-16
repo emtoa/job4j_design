9d4b752786a4060655cdede153c842eb2380f9ea
emtoa <emtoa@yandex.ru>
2020-08-16 21:27:06 +0300

    test

---
+++ chapter_001/src/main/java/myTest/Test1.java
@@ -0,0 +1,12 @@
+package myTest;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        int[][] in = {
+                {1}, {}, {}, {}, {2}
+        };
+        System.out.println(in[4].length);
+        System.out.println(in[4][0]);
+    }
+}
