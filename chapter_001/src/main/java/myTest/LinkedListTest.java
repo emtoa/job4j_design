c9a6899ced976638b0acb4b6283c49b42a690eb2
emtoa <emtoa@yandex.ru>
2020-09-05 23:39:49 +0300

    test

---
+++ chapter_001/src/main/java/myTest/LinkedListTest.java
@@ -0,0 +1,23 @@
+package myTest;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class LinkedListTest {
+
+    public static void main(String[] args) {
+        List<String> lists = new LinkedList<>();
+        lists.add("One");
+        lists.add("two");
+        lists.add("three");
+
+        lists.add(1,"error");
+
+        System.out.println(lists.get(0));
+        System.out.println(lists.get(1));
+        System.out.println(lists.get(2));
+
+    }
+}
---
+++ chapter_001/src/main/java/myTest/ListArrayTest.java
@@ -0,0 +1,22 @@
+package myTest;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListArrayTest {
+
+    public static void main(String[] args) {
+
+        List<String> lists = new ArrayList<>();
+        lists.add("One");
+        lists.add("two");
+        lists.add("three");
+
+        lists.add(1,"error");
+
+        System.out.println(lists.get(0));
+        System.out.println(lists.get(1));
+        System.out.println(lists.get(2));
+    }
+}
