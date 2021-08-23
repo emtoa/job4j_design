fca7b176672c38716d142b99d365a0b85a15faf2
emtoa <emtoa@yandex.ru>
2021-08-24 01:37:05 +0300

    0.2. FileInputStream [#4898]

--- chapter_001/src/main/java/ru/job4j/io/EvenNumberFile.java
+++ chapter_001/src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -5,23 +5,6 @@ import java.io.FileInputStream;
 public class EvenNumberFile {
 
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("io_file/event.txt")) {
-            StringBuilder text = new StringBuilder();
-            int read;
-            while ((read = in.read()) != -1) {
-                text.append((char) read);
-            }
-
-            String[] lines = text.toString().split(System.lineSeparator());
-
-            for (String line : lines) {
-                int result = Integer.parseInt(line.toString());
-                System.out.println(result % 2 == 0);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+     }
 }
 
