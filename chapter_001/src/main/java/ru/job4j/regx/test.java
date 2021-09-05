a9e4322a71813beeeee43c4c70d4e897d07f218e
emtoa <emtoa@yandex.ru>
2021-09-06 01:08:10 +0300

    test

--- chapter_001/src/main/java/ru/job4j/regx/test.java
+++ chapter_001/src/main/java/ru/job4j/regx/test.java
@@ -1,11 +1,37 @@
 package ru.job4j.regx;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.List;
+import java.util.StringJoiner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class test {
 
     public static void main(String[] args) {
+
+        List<String> words = List.of("foolish", "php");
+
+        String str  = "hello foolish dude java job4j php";
+        String wResult;
+        System.out.println(str);
+
+        str.lines().flatMap(line -> Stream.of(line.split("\\s+")))
+                .filter(word -> !words.contains(word))
+                .map(word -> word + " ")
+                .forEach(System.out::print);
+
+
+
+/*
+        StringJoiner sResult = new StringJoiner(";");
+
+        sResult.add("ddd");
+
+        System.out.println(sResult.length());
+
         Pattern pat;
         Matcher mat;
         boolean found;
@@ -18,5 +44,8 @@ public class test {
             System.out.println("Yes");
         else
             System.out.println("No");
+    }
+
+ */
     }
 }
