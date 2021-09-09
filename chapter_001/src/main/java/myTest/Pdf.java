c4accc2e72e41bbf0bd05d6974af8607c8e68695
emtoa <emtoa@yandex.ru>
2021-09-09 23:06:24 +0300

    test

--- chapter_001/src/main/java/myTest/Pdf.java
+++ chapter_001/src/main/java/myTest/Pdf.java
@@ -4,11 +4,6 @@ public class Pdf {
 
     public static void main(String[] args) {
 
-        PDFMergerUtility ut = new PDFMergerUtility();
-        ut.addSource(...);
-        ut.addSource(...);
-        ut.addSource(...);
-        ut.setDestinationFileName(...);
-        ut.mergeDocuments();
+
     }
 }
