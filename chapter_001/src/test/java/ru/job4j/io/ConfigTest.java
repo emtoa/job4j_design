83c2bc7335c22e69ae7eef79e70e37d5e884e621
emtoa <emtoa@yandex.ru>
2021-09-17 02:18:40 +0300

    test

--- chapter_001/src/test/java/ru/job4j/io/ConfigTest.java
+++ chapter_001/src/test/java/ru/job4j/io/ConfigTest.java
@@ -2,6 +2,7 @@ package ru.job4j.io;
 
 import org.junit.Test;
 
+import java.net.URL;
 import java.nio.file.Paths;
 import java.util.NoSuchElementException;
 
@@ -10,7 +11,7 @@ import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class ConfigTest {
-
+/*
     @Test
     public void whenPairWithComment() {
         String path = "./data/pair_without_comment.properties";
@@ -34,5 +35,15 @@ public class ConfigTest {
         Config config = new Config(path);
         config.load();
         assertThat(config.value(""), is("Aleksandr"));
+    }
+*/
+    @Test
+    public void whenPairWithComment1() {
+
+        URL path = ClassLoader.getSystemResource("./data/pair_without_comment.properties");
+        Config config = new Config(path.getPath());
+        config.load();
+        assertThat(config.value("name"), is("Aleksandr"));
+        assertThat(config.value("pass"), is("Figa"));
     }
 }
