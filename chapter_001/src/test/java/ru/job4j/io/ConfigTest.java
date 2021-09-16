e721c6fff2d871f9041b05cedb9e6b797accff0b
emtoa <emtoa@yandex.ru>
2021-09-17 02:25:08 +0300

    test

--- chapter_001/src/test/java/ru/job4j/io/ConfigTest.java
+++ chapter_001/src/test/java/ru/job4j/io/ConfigTest.java
@@ -14,8 +14,8 @@ public class ConfigTest {
 
     @Test
     public void whenPairWithComment() {
-        String path = "./data/pair_without_comment.properties";
-        Config config = new Config(path);
+        URL path = ClassLoader.getSystemResource("./data/pair_without_comment.properties");
+        Config config = new Config(path.getPath());
         config.load();
         assertThat(config.value("name"), is("Aleksandr"));
         assertThat(config.value("pass"), is("Figa"));
@@ -23,27 +23,17 @@ public class ConfigTest {
 
     @Test
     public void whenPairWithoutComment() {
-        String path = "./data/pair_without_comment.properties";
-        Config config = new Config(path);
+        URL path = ClassLoader.getSystemResource("./data/pair_without_comment.properties");
+        Config config = new Config(path.getPath());
         config.load();
         assertThat(config.value("ip"), is(nullValue()));
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void whenPairWithoutKey() {
-        String path = "./data/pair_without_comment.properties";
-        Config config = new Config(path);
-        config.load();
-        assertThat(config.value(""), is("Aleksandr"));
-    }
-
-    @Test
-    public void whenPairWithComment1() {
-
         URL path = ClassLoader.getSystemResource("./data/pair_without_comment.properties");
         Config config = new Config(path.getPath());
         config.load();
-        assertThat(config.value("name"), is("Aleksandr"));
-        assertThat(config.value("pass"), is("Figa"));
+        assertThat(config.value(""), is("Aleksandr"));
     }
 }
